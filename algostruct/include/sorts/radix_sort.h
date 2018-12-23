871bc62e951e35d6b9960c8fca1de968e8be1352
masdevas <mork.andrey.s.dev@gmail.com>
2018-12-23 21:09:29 +0300

    Remake Radix Sort.

--- algostruct/include/sorts/radix_sort.h
+++ algostruct/include/sorts/radix_sort.h
@@ -2,21 +2,28 @@
 #define ALGOSTRUCT_RADIX_SORT_H
 
 #include <iterator>
-#include <algorithm>
-#include <iostream>
+#include <vector>
 
-template <typename Iterator, typename DigitExtractor, typename Comparator>
-void RadixSort(Iterator begin, Iterator end, size_t count_digits, const DigitExtractor& extractor,
-               const Comparator& comparator) {
-    using ValueType =  typename std::iterator_traits<Iterator>::value_type;
+template <typename Iterator, typename DigitExtractor>
+void RadixSort(Iterator begin, Iterator end, size_t count_digits, size_t count_values,
+               const DigitExtractor& extractor) {
+    using ValueType = typename std::iterator_traits<Iterator>::value_type;
     size_t index_digit = 0;
-    auto comp = [&extractor, &index_digit, &comparator](const ValueType& value_left,
-                                                        const ValueType& value_right) {
-        return comparator(extractor(value_left, index_digit),
-                          extractor(value_right, index_digit));
-    };
     while (index_digit < count_digits) {
-        std::stable_sort(begin, end, comp);
+        std::vector<std::vector<ValueType>> buckets(count_values);
+        Iterator it = begin;
+        while (it != end) {
+            auto val = extractor(*it, index_digit);
+            buckets[val].emplace_back(*it);
+            ++it;
+        }
+        it = begin;
+        for (auto& bucket : buckets) {
+            for (auto& element : bucket) {
+                *it = element;
+                ++it;
+            }
+        }
         ++index_digit;
     }
 }
--- algostruct/tests/unit/sorts/radix_sort.cpp
+++ algostruct/tests/unit/sorts/radix_sort.cpp
@@ -1,7 +1,6 @@
 #include <gtest/gtest.h>
 #include <algorithm>
 #include <climits>
-#include <iostream>
 #include "sorts/radix_sort.h"
 #include "data_generation.h"
 
@@ -9,12 +8,9 @@ TEST(TEST_RADIX_SORT, RANDOM_TEST) {
     size_t size = 20000;
     using LocalDataType = uint8_t;
     using DataType = uint64_t;
-    DataType lower_bound = 0, upper_bound = INT64_MAX;
+    DataType lower_bound = 0, upper_bound = UINT64_MAX;
     auto data = GenerateRandomVector(size, lower_bound, upper_bound);
     auto copy_of_data = data;
-    auto comp_radix = [](const LocalDataType& data_first, const LocalDataType& data_second) {
-        return data_first < data_second;
-    };
     auto comp = [](const DataType& data_first, const DataType& data_second) {
         return data_first < data_second;
     };
@@ -22,7 +18,7 @@ TEST(TEST_RADIX_SORT, RANDOM_TEST) {
         DataType mask = static_cast<DataType>(~0) >> ((sizeof(DataType) - sizeof(LocalDataType)) * 8);
         return static_cast<LocalDataType>((data & (mask << 8 * sizeof(LocalDataType) * index_digit)) >> 8 * sizeof(LocalDataType) * index_digit);
     };
-    RadixSort(data.begin(), data.end(), sizeof(DataType) / sizeof(LocalDataType), extractor, comp_radix);
+    RadixSort(data.begin(), data.end(), sizeof(DataType) / sizeof(LocalDataType), UINT8_MAX + 1, extractor);
     std::sort(copy_of_data.begin(), copy_of_data.end(), comp);
     EXPECT_EQ(data, copy_of_data);
 }
