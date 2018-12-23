74261031e9f7f29f9e423a18b25c6a6d0958834e
masdevas <mork.andrey.s.dev@gmail.com>
2018-12-24 01:43:31 +0300

    Fix Bucket Sort.

--- algostruct/tests/unit/sorts/bucket_sort.cpp
+++ algostruct/tests/unit/sorts/bucket_sort.cpp
@@ -4,14 +4,14 @@
 #include "data_generation.h"
 
 TEST(TEST_BUCKET_SORT, RANDOM_TEST) {
-    size_t size = 20000;
-    DataType lower_bound = -1000, upper_bound = 1000;
+    size_t size = 100;
+    DataType lower_bound = INT32_MIN, upper_bound = INT32_MAX;
     auto data = GenerateRandomVector(size, lower_bound, upper_bound);
     auto copy_of_data = data;
     auto comp = [](const DataType& data_first, const DataType& data_second) {
         return data_first < data_second;
     };
-    BucketSort(data.begin(), data.end(), lower_bound, upper_bound + 1, comp);
+    BucketSort(data.begin(), data.end(), lower_bound, upper_bound, comp);
     std::sort(copy_of_data.begin(), copy_of_data.end(), comp);
     EXPECT_EQ(data, copy_of_data);
 }
