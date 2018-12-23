76b85fac6441d4c8f0f15c3b1eeb5460b40da637
masdevas <mork.andrey.s.dev@gmail.com>
2018-12-23 23:23:21 +0300

    Add code and create tests for K-Merge Sort.

--- algostruct/include/sorts/k_merge_sort.h
+++ algostruct/include/sorts/k_merge_sort.h
@@ -4,12 +4,65 @@
 #include <vector>
 #include <iterator>
 
+#include "bubble_sort.h"
+
+template <typename Iterator>
+struct Part {
+    Iterator begin;
+
+    Iterator end;
+};
+
+template <typename Iterator, typename Comparator>
+std::vector<Part<Iterator>> SortParts(std::vector<typename std::iterator_traits<Iterator>::value_type>& buffer,
+                                      const Comparator& comparator, size_t count_parts) {
+    size_t size_of_part = buffer.size() / count_parts;
+    std::vector<Part<Iterator>> parts(count_parts);
+    for (size_t part_number = 0; part_number < count_parts - 1; ++part_number) {
+        parts[part_number] = {buffer.begin() + size_of_part * part_number,
+                              buffer.begin() + size_of_part * (part_number + 1)};
+        BubbleSort(parts[part_number].begin, parts[part_number].end, comparator);
+    }
+    parts[count_parts - 1] = {buffer.begin() + size_of_part * (count_parts - 1), buffer.end()};
+    BubbleSort(parts[count_parts - 1].begin, parts[count_parts - 1].end, comparator);
+    return parts;
+}
+
+template <typename Iterator, typename Comparator>
+void MergeParts(Iterator begin, Iterator end, std::vector<Part<Iterator>>& parts,
+                const Comparator& comparator) {
+    auto limit = static_cast<size_t>(std::distance(begin, end));
+    for (size_t index_copying_element = 0; index_copying_element < limit;
+         ++index_copying_element) {
+        size_t current_checking_part = 0;
+        size_t number_best_part = 0;
+        while (current_checking_part < parts.size()) {
+            if (parts[current_checking_part].begin != parts[current_checking_part].end) {
+                number_best_part = current_checking_part;
+                break;
+            }
+            ++current_checking_part;
+        }
+        ++current_checking_part;
+        for (; current_checking_part < parts.size(); ++current_checking_part) {
+            if (parts[current_checking_part].begin != parts[current_checking_part].end) {
+                if (comparator(*(parts[current_checking_part].begin), *(parts[number_best_part].begin))) {
+                    number_best_part = current_checking_part;
+                }
+            }
+        }
+        *begin = *(parts[number_best_part].begin);
+        ++(parts[number_best_part].begin);
+        ++begin;
+    }
+}
 template <typename Iterator, typename Comparator>
 void KMergeSort(Iterator begin, Iterator end, const Comparator& comparator, size_t count_parts) {
     std::vector<typename std::iterator_traits<Iterator>::value_type> buffer(
             static_cast<size_t>(std::distance(begin, end)));
     std::copy(begin, end, buffer.begin());
-
+    auto parts = SortParts<Iterator>(buffer, comparator, count_parts);
+    MergeParts(begin, end, parts, comparator);
 }
 
 #endif
--- algostruct/tests/unit/sorts/k_merge_sort.cpp
+++ algostruct/tests/unit/sorts/k_merge_sort.cpp
@@ -4,14 +4,15 @@
 #include "data_generation.h"
 
 TEST(TEST_K_MERGE_SORT, RANDOM_TEST) {
-    size_t size = 20000;
-    DataType lower_bound = -1000, upper_bound = 1000;
+    size_t size = 30000;
+    DataType lower_bound = INT32_MIN, upper_bound = INT32_MAX;
+    size_t count_parts = 100;
     auto data = GenerateRandomVector(size, lower_bound, upper_bound);
     auto copy_of_data = data;
     auto comp = [](const DataType& data_first, const DataType& data_second) {
         return data_first < data_second;
     };
-    TreeSort(data.begin(), data.end(), comp);
+    KMergeSort(data.begin(), data.end(), comp, count_parts);
     std::sort(copy_of_data.begin(), copy_of_data.end(), comp);
     EXPECT_EQ(data, copy_of_data);
 }
